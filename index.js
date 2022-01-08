d02e0541844c7d8c6b79e01e7f908648cb9ad089
Luke Shiru <hi@lshi.ru>
2022-01-07 17:48:33 -0800

    :heavy_plus_sign: add `eslint-plugin-jsdoc`. :wrench: add rules for JSDocs.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-/*
-eslint-disable
-	@typescript-eslint/no-require-imports,
-	@typescript-eslint/no-var-requires,
-	functional/immutable-data,
-	functional/no-expression-statement,
-	max-lines,
-*/
-
+// eslint-disable-next-line functional/immutable-data
 module.exports = {
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	eslintrc: require("./.eslintrc.js"),
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	eslintrcReact: require("./.eslintrc.react.js"),
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	prettierrc: require("./.prettierrc.js"),
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	stylelintrc: require("./.stylelintrc.js"),
 };
