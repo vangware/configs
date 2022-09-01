bae197aaf3bbba77d0c2df1a521911f00281e4f4
Luke Shiru <hello@lukeshiru.dev>
2022-08-31 17:51:54 -0700

    :arrow_up: dependency upgrade. :wrench: update Prettier config for JSX/HTML/XML.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -4,38 +4,38 @@
  * @type {import("prettier").Options}
  */
 module.exports = {
-	// Avoid parentheses around a sole arrow function parameter
+	// Avoid parentheses around a sole arrow function parameter.
 	arrowParens: "avoid",
-	// Put the `>` of a multi-line HTML like in a new line
+	// Put the `>` of a multi-line HTML like in a new line.
 	bracketSameLine: false,
-	// Print spaces between brackets in object literals
+	// Print spaces between brackets in object literals.
 	bracketSpacing: true,
-	// Control whether Prettier formats quoted code embedded in the file
+	// Control whether Prettier formats quoted code embedded in the file.
 	embeddedLanguageFormatting: "auto",
-	// Unix EOL
+	// Unix EOL.
 	endOfLine: "lf",
-	// Ignore whitespace in HTML
+	// Ignore whitespace in HTML.
 	htmlWhitespaceSensitivity: "ignore",
-	// JSX should use double quote
+	// JSX should use double quote.
 	jsxSingleQuote: false,
-	// Print width is ideal at 80 characters
+	// Print width is ideal at 80 characters.
 	printWidth: 80,
-	// Wrap text to fit the print width
+	// Wrap text to fit the print width.
 	proseWrap: "always",
-	// Add quotes around properties of object if needed
+	// Add quotes around properties of object if needed.
 	quoteProps: "as-needed",
 	// Print semicolons at the ends of statements.
 	semi: true,
-	// Shows one attribute per line in HTML, Vue and JSX
-	singleAttributePerLine: true,
-	// Use double quotes
+	// Shows multiple attributes per line in HTML, Vue and JSX.
+	singleAttributePerLine: false,
+	// Use double quotes.
 	singleQuote: false,
-	// Tab width at 4 is ideal
+	// Tab width at 4 is ideal.
 	tabWidth: 4,
-	// Add trailing commas to make edition easier and diffing better
+	// Add trailing commas to make edition easier and diffing better.
 	trailingComma: "all",
-	// Obviously use tabs
+	// Obviously use tabs.
 	useTabs: true,
-	// Indent code in Vue files
+	// Indent code in Vue files.
 	vueIndentScriptAndStyle: true,
 };
