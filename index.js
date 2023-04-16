6c0ef8248c2e82253941810fd3aa91e7412f3151
Luke Shiru <hi@luke.sh>
2023-04-15 22:47:22 -0700

    🐛 fix exports for JSON files.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import typedocConfig from "./typedoc.json" assert { type: "json" };
+import typescriptConfig from "./typescript.config.json" assert { type: "json" };
+
 export { default as eslintConfig } from "./eslint.config.js";
 export { default as prettierConfig } from "./prettier.config.cjs";
 export { default as stylelintConfig } from "./stylelint.config.cjs";
-export { default as typedocConfig } from "./typedoc.json" assert { type: "json" };
-export { default as typescriptConfig } from "./typescript.config.json" assert { type: "json" };
+export { typedocConfig, typescriptConfig };
