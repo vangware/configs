965efa9b07745758b57c2fe7dd0652fcc828cc31
Lou Cyx <git@lou.cx>
2023-04-15 22:47:13 -0700

    :heavy_minus_sign: remove `pre-push`. :heavy_plus_sign: add `@evilmartians/lefthook`.

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as eslintrc } from "./.eslintrc.cjs";
+export { default as eslintrcReact } from "./.eslintrc.react.cjs";
+export { default as prettierrc } from "./.prettierrc.cjs";
+export { default as stylelintrc } from "./.stylelintrc.cjs";
