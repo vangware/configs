d9704d48d184d6b837d3667f93d122aa324cd382
Lou Cyx <git@lou.cx>
2023-04-15 22:46:27 -0700

    :tada: initial commit.

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+eslint-disable
+	@typescript-eslint/no-require-imports,
+	@typescript-eslint/no-var-requires,
+	functional/immutable-data,
+	functional/no-expression-statement,
+	max-lines,
+*/
+
+module.exports = {
+	eslintrc: require("./.eslintrc.js"),
+	eslintrcReact: require("./.eslintrc.react.js"),
+	prettierrc: require("./.prettierrc.js"),
+	stylelintrc: require("./.stylelintrc.js"),
+};
